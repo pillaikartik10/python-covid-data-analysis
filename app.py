67d90451a53e94653a62dd86ef8a566971ff972b
pillaikartik10 <69299211+pillaikartik10@users.noreply.github.com>
2021-05-14 21:28:52 +0530

    Source code for webscraping in Python

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
+#scraping data from the website
+
 html = requests.get('https://www.worldometers.info/coronavirus/').text
 html_soup = BeautifulSoup(html, 'html.parser')
 rows = html_soup.find_all('tr')
+
+
+#function to extract text
 
 def extract_text(row, tag):
     element = BeautifulSoup(row, 'html.parser').find_all(tag)
@@ -12,7 +17,12 @@ def extract_text(row, tag):
     return text
 
 heading = rows.pop(0)
+
+#extracting columns 1- 8
+
 heading_row = extract_text(str(heading), 'th')[1:9]
+
+#function to store the scraped data into a .csv file
 
 def test():
     with open('corona.csv', 'w') as store:
